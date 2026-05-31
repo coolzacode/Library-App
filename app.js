89ec952d7b5c841837e313d1cfc669a8af20d1d6
hazzaah <154761475+hazzaah@users.noreply.github.com>
2026-05-31 12:43:45 -0500

    feat: open and close dialog

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const addBtn = document.getElementById('add-btn');
+const cancelBtn = document.getElementById('cancel-btn');
 const dialog = document.getElementById('book-modal');
+const form = document.getElementById('book-form');
 
-function handleClick() {
-    dialog.showModal();
-}
+const library = [];
 
 
-addBtn.addEventListener('click', handleClick);
+
+
+addBtn.addEventListener('click', () => dialog.showModal());
+cancelBtn.addEventListener('click', () => {
+    form.reset();
+    dialog.close();
+})
